0b08fc1e7da6154ac228d74e53a3505bffc05841
ho9science <full17@naver.com>
2021-10-06 23:31:48 +0900

    Finished Gatsby Tutorial Part3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "mididice-808",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
